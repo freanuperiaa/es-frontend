85e16731b61de70de8fb176953956424c5e28f2a
Freanu Peria <magnussonfrancis@gmail.com>
2021-06-04 13:14:23 +0800

    added about page.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 import './assets/styles/App.css';
 import Navbar from "./components/Navbar.js"
 import Home from "./components/home/Home.js"
+import About from "./components/about/About.js"
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
         <Container>
             <Row className="mt-5 justify-content-center">
                 <Col xs={11} md={10} lg={8} xl={8}>
-                    <Home />
+                    <About />
                 </Col>
             </Row>
         </Container>
---
+++ src/components/about/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "../../assets/styles/About.css"
+
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <p className="about-one">
+                <span className="howimfeelingnow">howimfeelingnow.</span>
+                <br />
+                 is an app for those people who feel like  there's 
+                 <br/>
+                 <span className="howimfeelingnow">_something wrong with their emotions, behaviours, and feelings</span>_. 
+                 <br/>
+                 <br/>
+                <span className="howimfeelingnow">howimfeelingnow.</span> is an app for those people who feel &nbsp;
+                <span className="sad">sad</span>, &nbsp;
+                <span className="depressed">depressed</span>, &nbsp;
+                <span className="perturbed">perturbed</span>, &nbsp;
+                <span className="something-wrong">like there's something wrong</span>, &nbsp;
+                <span className="overly-happy">overly happy</span>, &nbsp;
+                <span className="suicidal">suicidal.</span> &nbsp;
+                <br />
+                <br />
+                Read more about <span className="mental-health">Mental Health</span> &nbsp;
+                <a 
+                    href="https://www.mentalhealth.gov/basics/what-is-mental-health"
+                    className ="link"
+                >
+                    here
+                </a>.
+            </p>
+
+            <p className="about-two">
+                This is a project for CS 404 - Expert Systems, developed by Mickaela Jeorge Mendoza, Mark Alfie Mercado, and Freanu Peria.
+            </p>
+        </div>
+    )
+}
+
+export default About;
