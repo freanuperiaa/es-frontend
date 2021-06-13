4c50c31d34a1c4de1c1c7cab4e4318f8df991d9b
Freanu Peria <magnussonfrancis@gmail.com>
2021-06-13 16:38:26 +0800

    setup questions. answers WIP.

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button} from "react-bootstrap";
 import {Link, useParams} from "react-router-dom"
 
@@ -9,26 +9,56 @@ import {questions} from "../../utils/generic.utils.js"
 const Test = () => {
 
     let {category} = useParams();
+    let [curQuestionIndex, setCurQuestionIndex] = useState(0);
+
+    let previousQuestion = () => {
+        let newCurrentQuestionIndex = curQuestionIndex - 1;
+        setCurQuestionIndex(newCurrentQuestionIndex)
+    }
+
+    let nextQuestion = () => {
+        let newCurrentQuestionIndex = curQuestionIndex + 1;
+        setCurQuestionIndex(newCurrentQuestionIndex)
+    }
 
     /**
      * let the static indexing ("depression") below be until the questions and categories are already set.
      */
 
-    let currentQuestions = questions["depression"]
-    console.log(currentQuestions)
+    let currentQuestions = questions["depression"].questions
+    let totalScore = questions["depression"].totalScore
 
-    console.log()
     
     return (
         <div className="test-container">
             <p className="title">{category} Test</p>
-            <p className="sub-title">For the past two weeks, tell us to what extent you have experienced the following things.</p>
+            <p className="sub-title">For the past two weeks, tell us to what extent you have experienced the following things:</p>
+
+            <div className="question-answer">
+                <p className="question">
+                    {currentQuestions[curQuestionIndex].question}
+                </p>
+
+                <div className="answers">
+                    
+                </div>
+            </div>
 
             <div className="buttons-row">
-                <Button className="previous-button">Previous</Button>
-                <Link to="/test/results">
-                    <Button className="next-button">Next</Button>
-                </Link>
+                <Button 
+                    className="previous-button"
+                    onClick={previousQuestion}
+                >
+                    Previous
+                </Button>
+
+                <Button 
+                    className="next-button"
+                    onClick={nextQuestion}
+                >
+                    Next
+                </Button>
+                
             </div>
 
         </div>
